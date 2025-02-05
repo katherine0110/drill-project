4588fd44d15b15ff2698b412add507f91f6916a9
katherine <katherine.chiu68@gmail.com>
2025-02-06 00:16:49 +0800

    update placing squad

--- src/app/common/squad-util.ts
+++ src/app/common/squad-util.ts
@@ -14,8 +14,9 @@ export class SquadFunc {
   calculateSquad(markerPosition: Position, formation: formation, frontMarker: boolean, originSquad: Position[]): Position[] {
     const data = this.getNewSquad(markerPosition, formation, frontMarker);
     //sort in descending order
-    const xList = data.sort((a, b) => b.x - a.x);
-    const yList = data.sort((a, b) => b.y - a.y);
+    const tempList = [...data];
+    const xList = tempList.sort((a, b) => b.x - a.x);
+    const yList = tempList.sort((a, b) => b.y - a.y);
     if (xList[0].x > 63 || yList[0].y > 63 || xList[11].x < 0 || yList[11].y < 0) {
       return originSquad;
     }
@@ -24,73 +25,81 @@ export class SquadFunc {
 
   getNewSquad(markerPosition: Position, formation: formation, frontMarker: boolean): Position[] {
     let squadPosition: Position[];
+    let squadFacing = markerPosition.facing;
+
     if (
-      (formation === 'column' && (markerPosition.facing === 'up' || markerPosition.facing === 'down')) ||
-      (formation === 'line' && (markerPosition.facing === 'left' || markerPosition.facing === 'right'))
+      (frontMarker && ((formation === 'line' && squadFacing === 'up') || (formation === 'column' && squadFacing === 'right'))) ||
+      (!frontMarker && ((formation === 'line' && squadFacing === 'down') || (formation === 'column' && squadFacing === 'left')))
     ) {
-      if (frontMarker) {
-        squadPosition = [
-          { x: markerPosition.x, y: markerPosition.y, facing: markerPosition.facing},
-          { x: markerPosition.x + 3, y: markerPosition.y, facing: markerPosition.facing },
-          { x: markerPosition.x + 6, y: markerPosition.y, facing: markerPosition.facing },
-          { x: markerPosition.x + 9, y: markerPosition.y, facing: markerPosition.facing },
-          { x: markerPosition.x, y: markerPosition.y + 3, facing: markerPosition.facing },
-          { x: markerPosition.x + 3, y: markerPosition.y + 3, facing: markerPosition.facing },
-          { x: markerPosition.x + 6, y: markerPosition.y + 3, facing: markerPosition.facing },
-          { x: markerPosition.x + 9, y: markerPosition.y + 3, facing: markerPosition.facing },
-          { x: markerPosition.x, y: markerPosition.y + 6, facing: markerPosition.facing },
-          { x: markerPosition.x + 3, y: markerPosition.y + 6, facing: markerPosition.facing },
-          { x: markerPosition.x + 6, y: markerPosition.y + 6, facing: markerPosition.facing },
-          { x: markerPosition.x + 9, y: markerPosition.y + 6, facing: markerPosition.facing },
-        ];
-      } else {
-        squadPosition = [
-          { x: markerPosition.x, y: markerPosition.y, facing: markerPosition.facing},
-          { x: markerPosition.x - 3, y: markerPosition.y, facing: markerPosition.facing },
-          { x: markerPosition.x - 6, y: markerPosition.y, facing: markerPosition.facing },
-          { x: markerPosition.x - 9, y: markerPosition.y, facing: markerPosition.facing },
-          { x: markerPosition.x, y: markerPosition.y - 3, facing: markerPosition.facing },
-          { x: markerPosition.x - 3, y: markerPosition.y - 3, facing: markerPosition.facing },
-          { x: markerPosition.x - 6, y: markerPosition.y - 3, facing: markerPosition.facing },
-          { x: markerPosition.x - 9, y: markerPosition.y - 3, facing: markerPosition.facing },
-          { x: markerPosition.x, y: markerPosition.y - 6, facing: markerPosition.facing },
-          { x: markerPosition.x - 3, y: markerPosition.y - 6, facing: markerPosition.facing },
-          { x: markerPosition.x - 6, y: markerPosition.y - 6, facing: markerPosition.facing },
-          { x: markerPosition.x - 9, y: markerPosition.y - 6, facing: markerPosition.facing },
-        ];
-      }
-    } else {
-      if (frontMarker) {
-        squadPosition = [
-          { x: markerPosition.x, y: markerPosition.y, facing: markerPosition.facing},
-          { x: markerPosition.x, y: markerPosition.y - 3, facing: markerPosition.facing },
-          { x: markerPosition.x, y: markerPosition.y - 6, facing: markerPosition.facing },
-          { x: markerPosition.x, y: markerPosition.y - 9, facing: markerPosition.facing },
-          { x: markerPosition.x + 3, y: markerPosition.y, facing: markerPosition.facing },
-          { x: markerPosition.x + 3, y: markerPosition.y - 3, facing: markerPosition.facing },
-          { x: markerPosition.x + 3, y: markerPosition.y - 6, facing: markerPosition.facing },
-          { x: markerPosition.x + 3, y: markerPosition.y - 9, facing: markerPosition.facing },
-          { x: markerPosition.x + 6, y: markerPosition.y, facing: markerPosition.facing },
-          { x: markerPosition.x + 6, y: markerPosition.y - 3, facing: markerPosition.facing },
-          { x: markerPosition.x + 6, y: markerPosition.y - 6, facing: markerPosition.facing },
-          { x: markerPosition.x + 6, y: markerPosition.y - 9, facing: markerPosition.facing },
-        ];
-      } else {
-        squadPosition = [
-          { x: markerPosition.x, y: markerPosition.y, facing: markerPosition.facing},
-          { x: markerPosition.x, y: markerPosition.y + 3, facing: markerPosition.facing },
-          { x: markerPosition.x, y: markerPosition.y + 6, facing: markerPosition.facing },
-          { x: markerPosition.x, y: markerPosition.y + 9, facing: markerPosition.facing },
-          { x: markerPosition.x - 3, y: markerPosition.y, facing: markerPosition.facing },
-          { x: markerPosition.x - 3, y: markerPosition.y + 3, facing: markerPosition.facing },
-          { x: markerPosition.x - 3, y: markerPosition.y + 6, facing: markerPosition.facing },
-          { x: markerPosition.x - 3, y: markerPosition.y + 9, facing: markerPosition.facing },
-          { x: markerPosition.x - 6, y: markerPosition.y, facing: markerPosition.facing },
-          { x: markerPosition.x - 6, y: markerPosition.y + 3, facing: markerPosition.facing },
-          { x: markerPosition.x - 6, y: markerPosition.y + 6, facing: markerPosition.facing },
-          { x: markerPosition.x - 6, y: markerPosition.y + 9, facing: markerPosition.facing },
-        ];
-      }
+      //line with marker at top
+      squadPosition = [
+        { x: markerPosition.x, y: markerPosition.y, facing: squadFacing},
+        { x: markerPosition.x, y: markerPosition.y - 3, facing: squadFacing },
+        { x: markerPosition.x, y: markerPosition.y - 6, facing: squadFacing },
+        { x: markerPosition.x, y: markerPosition.y - 9, facing: squadFacing },
+        { x: markerPosition.x + 3, y: markerPosition.y, facing: squadFacing },
+        { x: markerPosition.x + 3, y: markerPosition.y - 3, facing: squadFacing },
+        { x: markerPosition.x + 3, y: markerPosition.y - 6, facing: squadFacing },
+        { x: markerPosition.x + 3, y: markerPosition.y - 9, facing: squadFacing },
+        { x: markerPosition.x + 6, y: markerPosition.y, facing: squadFacing },
+        { x: markerPosition.x + 6, y: markerPosition.y - 3, facing: squadFacing },
+        { x: markerPosition.x + 6, y: markerPosition.y - 6, facing: squadFacing },
+        { x: markerPosition.x + 6, y: markerPosition.y - 9, facing: squadFacing },
+      ];
+    }else if (
+      (frontMarker && ((formation === 'line' && squadFacing === 'down') || (formation === 'column' && squadFacing === 'left'))) ||
+      (!frontMarker && ((formation === 'line' && squadFacing === 'up') || (formation === 'column' && squadFacing === 'right')))
+    ) {
+      //line with marker at bottom
+      squadPosition = [
+        { x: markerPosition.x, y: markerPosition.y, facing: squadFacing},
+        { x: markerPosition.x, y: markerPosition.y + 3, facing: squadFacing },
+        { x: markerPosition.x, y: markerPosition.y + 6, facing: squadFacing },
+        { x: markerPosition.x, y: markerPosition.y + 9, facing: squadFacing },
+        { x: markerPosition.x - 3, y: markerPosition.y, facing: squadFacing },
+        { x: markerPosition.x - 3, y: markerPosition.y + 3, facing: squadFacing },
+        { x: markerPosition.x - 3, y: markerPosition.y + 6, facing: squadFacing },
+        { x: markerPosition.x - 3, y: markerPosition.y + 9, facing: squadFacing },
+        { x: markerPosition.x - 6, y: markerPosition.y, facing: squadFacing },
+        { x: markerPosition.x - 6, y: markerPosition.y + 3, facing: squadFacing },
+        { x: markerPosition.x - 6, y: markerPosition.y + 6, facing: squadFacing },
+        { x: markerPosition.x - 6, y: markerPosition.y + 9, facing: squadFacing },
+      ];
+    }else if (
+      (frontMarker && ((formation === 'column' && squadFacing === 'up') || (formation === 'line' && squadFacing === 'left'))) ||
+      (!frontMarker && ((formation === 'column' && squadFacing === 'down') || (formation === 'line' && squadFacing === 'right')))
+    ) {
+      //column with marker at top
+      squadPosition = [
+        { x: markerPosition.x, y: markerPosition.y, facing: squadFacing},
+        { x: markerPosition.x + 3, y: markerPosition.y, facing: squadFacing },
+        { x: markerPosition.x + 6, y: markerPosition.y, facing: squadFacing },
+        { x: markerPosition.x + 9, y: markerPosition.y, facing: squadFacing },
+        { x: markerPosition.x, y: markerPosition.y + 3, facing: squadFacing },
+        { x: markerPosition.x + 3, y: markerPosition.y + 3, facing: squadFacing },
+        { x: markerPosition.x + 6, y: markerPosition.y + 3, facing: squadFacing },
+        { x: markerPosition.x + 9, y: markerPosition.y + 3, facing: squadFacing },
+        { x: markerPosition.x, y: markerPosition.y + 6, facing: squadFacing },
+        { x: markerPosition.x + 3, y: markerPosition.y + 6, facing: squadFacing },
+        { x: markerPosition.x + 6, y: markerPosition.y + 6, facing: squadFacing },
+        { x: markerPosition.x + 9, y: markerPosition.y + 6, facing: squadFacing },
+      ];
+    }else {
+      //column with marker at bottom
+      squadPosition = [
+        { x: markerPosition.x, y: markerPosition.y, facing: squadFacing},
+        { x: markerPosition.x - 3, y: markerPosition.y, facing: squadFacing },
+        { x: markerPosition.x - 6, y: markerPosition.y, facing: squadFacing },
+        { x: markerPosition.x - 9, y: markerPosition.y, facing: squadFacing },
+        { x: markerPosition.x, y: markerPosition.y - 3, facing: squadFacing },
+        { x: markerPosition.x - 3, y: markerPosition.y - 3, facing: squadFacing },
+        { x: markerPosition.x - 6, y: markerPosition.y - 3, facing: squadFacing },
+        { x: markerPosition.x - 9, y: markerPosition.y - 3, facing: squadFacing },
+        { x: markerPosition.x, y: markerPosition.y - 6, facing: squadFacing },
+        { x: markerPosition.x - 3, y: markerPosition.y - 6, facing: squadFacing },
+        { x: markerPosition.x - 6, y: markerPosition.y - 6, facing: squadFacing },
+        { x: markerPosition.x - 9, y: markerPosition.y - 6, facing: squadFacing },
+      ];
     }
     return squadPosition;
 }
